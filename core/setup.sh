b1549af25b78fe79bceaad60c950ced1b155890e
bf4r <sharpslpcz@gmail.com>
2025-01-12 10:10:45 +0100

    Fix modules not being found

--- core/setup.sh
+++ core/setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-TUSH_PATH="../"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TUSH_PATH="$SCRIPT_DIR/.."
 DATA_PATH="$TUSH_PATH/data"
 CONFIG_PATH="$TUSH_PATH/config"
