956a7b52529d63615e165000c8763e444178b187
bf4r <sharpslpcz@gmail.com>
2025-01-12 11:31:19 +0100

    Add one-time execution, interpret method, arguments to module

--- core/tu.sh
+++ core/tu.sh
@@ -3,14 +3,35 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $SCRIPT_DIR/setup.sh
 
+# $1 = module name
+# everything else = arguments to module
+# tush <module_name> <arguments>
+interpret() {
+  local module_name=$1
+  module_file="$TUSH_PATH/modules/$module_name.sh"
+  if [ -f $module_file ]; then
+    # Execute the module script with arguments to tush
+    source "$module_file" "$@"
+  else
+    echo "Unknown module: $1"
+  fi
+}
+
+# At least 1 argument = interpret the command and exit
+if [ $# -gt 0 ]; then
+  interpret "$@"
+  exit 0
+fi
+
+# Main loop if no arguments
 while true
 do
   echo -n "> "
   read input
-  module_file="$TUSH_PATH/modules/$input.sh"
-  if [ -f $module_file ]; then
-    source $module_file
-  else
-    echo "Unknown: $input"
+  if [ -z "$input" ]; then
+    continue
+  elif [ "$input" = "exit" ]; then
+    break
   fi
+  interpret $input
 done
--- tu.sh
+++ tu.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $SCRIPT_DIR/core/tu.sh
+source $SCRIPT_DIR/core/tu.sh $*
