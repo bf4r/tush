79eb60b5f68c7985af5117f4be32fb11c3bb1591
bf4r <sharpslpcz@gmail.com>
2025-01-13 10:55:31 +0100

    Update module template

--- templates/module/main.sh
+++ templates/module/main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $SCRIPT_DIR/config.sh
+source $SCRIPT_DIR/config.sh # Remove these 2 lines if you don't have a config.sh
 # Your module script here
 
